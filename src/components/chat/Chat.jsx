67b3add5a8a292d868d6849c1abe4221866be09d
TBuscemi <thomasbuscemi124@gmail.com>
2021-09-24 03:22:25 -0400

    Chat  login works still need make the creat part of it and add some css to it over all+Footer is taken out for now since it blocks the chat. I will need add the footer to each poage and import it except on chat page to get it to work,

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,15 +1,17 @@
-import { ChatEngine} from 'react-chat-engine';
+import { ChatEngine} from 'react-chat-engine'
+import LoginAndCreate from './loginAndCreate/LoginAndCreate';
 import './Chat.css'
 
 
 
 const Chat = ( ) =>{
+    if (!localStorage.getItem('username')) return <LoginAndCreate />;
     return (
         <ChatEngine
             height="100vh"
             projectID="bc20eb8f-cb09-48db-80bc-fdb34b544181"
-            userName="GameMaster"
-            userSecret= "Sweeper124"
+            userName="Mid"   //{localStorage.getItem('username')}
+            userSecret="Sweeper124"    //{localStorage.getItem('password')
         />
     );
 };
